9cc5e622bb824fb2b56fb69ef7ce0a913aa068e6
joseademar <joseademarti@gmail.com>
2023-03-29 21:28:22 -0300

    Estudando variaveis no JavaScript

---
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,20 @@
+let js = 'Amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Junior');
+console.log(23);
+
+let firstName = 'Junior';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCureentJob = 'Teacher';
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+
