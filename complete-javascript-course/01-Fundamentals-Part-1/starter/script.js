678e241d268abbf34054c7556f5cd159c800161f
joseademar <joseademarti@gmail.com>
2023-06-01 21:38:02 -0300

    Estudando os tipos falso do javascript

--- complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -154,6 +154,7 @@ console.log(BMIMARK,BMIJOHN);
 // Convertion and coersion
 // NaN é a number porém um tipo de número invalido
 
+/*
 const inputYear = '1991';
 console.log(Number(inputYear), inputYear);  //Convertendo string para numero
 
@@ -163,3 +164,27 @@ valor = 23;
 console.log(String(valor) + 1); //Convertendo número para string
 
 console.log('23' - '10' - 3); //Quando temos string com o operador de menos as strings é convertida em número e a subtração é realizada.
+*/
+
+// 5 tipos falso do javascript
+// valores:  0, '', undefined, null, NaN
+
+console.log(Boolean(0))
+console.log(Boolean(undefined))
+console.log(Boolean('Jonas'))
+console.log(Boolean({}))
+console.log(Boolean(''))
+
+const money = 0;
+if(money){
+    console.log("Don't spend it all");
+}else{
+    console.log("You should get a job");
+}
+
+let height;
+if(height){
+    console.log("yay! height is defined");
+}else{
+    console.log("height is undefined");
+}
