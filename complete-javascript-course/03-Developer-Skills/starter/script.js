519168a15a12a1a961df562fdb93beca898883c3
joseademar <joseademarti@gmail.com>
2024-01-31 16:16:40 -0300

    Resolução de exercicios, estudando debug conceitos básico de HTML & CSS e também conceitos sobre DOM o que DOM e criando o primeiro projeto com HTML, CSS & javascript

--- complete-javascript-course/03-Developer-Skills/starter/script.js
+++ complete-javascript-course/03-Developer-Skills/starter/script.js
@@ -36,6 +36,7 @@ const amplitude = calcTempAmplitude(temperatures);
 console.log(amplitude);
 */
 
+/*
 const calcTempAmplitudeNew = function (t1, t2) {
     let t3 = t1.concat(t2);
     let max = t3[0];
@@ -60,3 +61,58 @@ const calcTempAmplitudeNew = function (t1, t2) {
 
 const amplitude = calcTempAmplitudeNew(temperatures, temperatures2);
 console.log(amplitude);
+*/
+
+/*
+const measureKelvin = function () {
+    const measurement = {
+        type: 'temp',
+        unit: 'celcius',
+        value: Number(prompt('Degrees celsius:')),
+    };
+
+    const kelvin = measurement.value + 273;
+    return kelvin;
+}
+
+console.log(measureKelvin());
+*/
+
+/*
+const calcTempAmplitudeBug = function (t1, t2) {
+    let t3 = t1.concat(t2);
+    let max = t3[0];
+    let min = t3[0];
+
+    for (let i = 0; i < t3.length; i++) {
+        const curTemp = t3[i];
+        if (typeof curTemp !== "number") continue;
+        if (curTemp > max) {
+            max = curTemp;
+        }
+        if (curTemp < min) {
+            min = curTemp;
+        }
+    }
+
+    console.log("Max tem aplitude " + max);
+    console.log("Min tem aplitude " + min);
+    console.log(t3);
+    return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+console.log(amplitudeBug);
+*/
+
+const printForecast = function (arr) {
+    let str = '';
+    for (let i = 0; i < arr.length; i++) {
+        str += `${arr[i]}°C in ${i + 1} days ... `;
+    }
+
+    console.log('..' + str);
+}
+
+printForecast([17, 21, 23]);
+printForecast([12, 5, -5, 0, 4]);
