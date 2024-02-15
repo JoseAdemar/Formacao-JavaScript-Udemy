381d203f55f339d348c1e33ab28f11a7dfa3e27d
joseademar <joseademarti@gmail.com>
2024-02-14 21:31:07 -0300

    Trabalhando na implementação da regra de negócio do jogo.

--- complete-javascript-course/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course/05-Guess-My-Number/starter/script.js
@@ -1,3 +1,41 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '✅ Correct Number!'
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+const toValidScore = function () {
+    if (score === 0) {
+        document.querySelector('.message').textContent = '💥😕 You lost the game!';
+    }
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔ Invalid Number!';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '✅ Correct Number, Congratulations!';
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = '📈 Too high!';
+        score--;
+        document.querySelector('.score').textContent = score;
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = '📉 Too low!';
+        score--;
+        document.querySelector('.score').textContent = score;
+    }
+
+    toValidScore();
+});
